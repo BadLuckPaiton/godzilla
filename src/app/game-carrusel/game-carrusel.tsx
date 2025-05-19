e258afeb80413aadc5a2e950a6cf3566d413b21a
Chuy chaidez <chuyelpatonchaidez@gmail.com>
2025-05-18 23:46:45 -0700

    Adding Homepage Structure in different Documents

---
+++ src/app/game-carrusel/game-carrusel.tsx
@@ -0,0 +1,21 @@
+import './game-carrusel.css'
+import Game from '../components/game/game'
+
+export default function GameCarrusel(){
+    return(
+        <div className="games-carrusel">
+        <h1>New Games</h1>
+        <h1>-----------------------------------------------------</h1>
+        <div className="list-games">
+          <Game/>             
+          <Game/>             
+          <Game/>             
+          <Game/>             
+          <Game/>             
+          <Game/>             
+          <Game/>             
+          <Game/>             
+        </div>
+      </div>
+    );
+}
--- src/app/home/homepage.tsx
+++ src/app/home/homepage.tsx
@@ -1,43 +1,19 @@
 import Image from "next/image";
 import './homepage.css'
+import '../promotion-carrusel/promotion-carrusel'
+import PromotionCarrusel from "../promotion-carrusel/promotion-carrusel";
+import GameCarrusel from "../game-carrusel/game-carrusel";
 
 export default function HomePage(){
     return(
       <div >
         <main className="flex flex-col gap-8 row-start-2 items-center">
           <div className="home-page">
-            <div className="promotion-carrusel" />
-            <div className="new-game">
-              <h1>New Games</h1>
-              <h1>-----------------------------------------------------</h1>
-              <div className="list-games">
-                <div className="game" />
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-              </div>
-            </div>
-            <div className="new-game">
-            <h1>Friends Playing</h1>
-              <h1>-----------------------------------------------------</h1>
-              <div className="list-games">
-                <div className="game" />
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-                <div className="game" />                
-              </div>
-            </div>
-            <div className="promotion-carrusel" />
+            <PromotionCarrusel />
+            <GameCarrusel />
+            <GameCarrusel />
+            <PromotionCarrusel />
+
             <div className="review-list">  
               <div className="review" />
             </div>
